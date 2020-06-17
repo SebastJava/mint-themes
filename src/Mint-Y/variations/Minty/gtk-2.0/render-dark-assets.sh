2981c0bcf5526c76b87a62cb731f3600c0baa626
SebastJava <sebastjava@hotmail.ca>
2020-06-17 03:34:01 -0400

    modified:   .gitignore

---
+++ src/Mint-Y/variations/Minty/gtk-2.0/render-dark-assets.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+INKSCAPE="/usr/bin/inkscape"
+OPTIPNG="/usr/bin/optipng"
+
+SRC_FILE="assets-dark.svg"
+ASSETS_DIR="assets-dark"
+INDEX="assets.txt"
+
+for i in `cat $INDEX`
+do
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-filename=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
+fi
+done
+
+cp $ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-dark.png
+cp $ASSETS_DIR/toolbar-entry-border-active-bg.png menubar-toolbar/toolbar-entry-border-active-bg-dark.png
+
+exit 0
